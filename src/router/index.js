ef6872e358642704f0564e86c5dd6d24449a0f83
Thebearcoding <1193617419@qq.com>
2023-10-15 21:53:05 +0800

    更新登录注册

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes:[{
-        
+    routes:[
+        {
             path: '/',
-            name: '',
+            name: 'Home',
+            component: ()=> import('../views/home/Home.vue')
+        },
+        {
+            path: '/login',
+            name: "Login",
             component: ()=> import('../views/account/Login.vue')
-    }]
+        }
+]
 })
 export default router;
