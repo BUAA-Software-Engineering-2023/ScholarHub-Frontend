659ad9b58cd9c128c607823ed7d025558f25690c
Thebearcoding <1193617419@qq.com>
2023-11-28 19:10:36 +0800

    更新paper论文详情

--- src/views/paper/Paper.vue
+++ src/views/paper/Paper.vue
@@ -1,11 +1,399 @@
 <script setup>
+import SearchAPI from "@/api/search.js"
+import {useRoute} from "vue-router";
+import ReferenceWork from "@/views/paper/ReferenceWork.vue";
+const paperInfo =ref([])
+const route = useRoute()
+const authorInfo = ref()
+const download = ref(false)
+const paperId = "https://openalex.org/"+route.params.paperId
+const reference_works = ref([]);
+onMounted(async () => {
+  const result = await SearchAPI.get_article_detail(paperId);
+  if (result.data.success){
+    console.log(result.data.data)
+    paperInfo.value = [result.data.data]
+  }
+  reference_works.value = paperInfo.value[0].referenced_works
+  console.log(reference_works.value)
+});
+const referencedWorks = ref([
+  {
+    id: 1,
+    display_name: "参考文献标题1",
+    publication_year: 2020
+    // 可以在这里添加其他字段的信息
+  },
+  {
+    id: 2,
+    display_name: "参考文献标题2",
+    publication_year: 2019
+    // 可以在这里添加其他字段的信息
+  },
+  // 可以根据需要继续添加其他参考文献的信息
+]);
+function showAuthorInfo(author) {
+  authorInfo.value = author;
+}
+function downloadPDF(){
+  const url = paperInfo.value[0].open_access.oa_url // 下载文件的url
+  const link = document.createElement('a') ;
+  link.href = url;
+  link.download = 'file.pdf' // 下载文件的名称
+  link.target="_blank"
+  link.click()
+  console.log(paperInfo.value[0].open_access.oa_url)
+}
+function hideAuthorInfo() {
+  authorInfo.value = null;
+}
+const showAbstract = ref(false);
 
+function toggleAbstract() {
+  showAbstract.value = !showAbstract.value;
+}
+function showDownload(){
+  download.value = true;
+}
+function hideDownload(){
+  download.value = false;
+}
 </script>
 
 <template>
+  <div class="main-container">
+    <div class="content">
+      <div class="paper">
+        <div v-for="(paper, index) in paperInfo" :key="index" class="paper-item">
+          <div  @click="jumpToarticle"> <span class="paper-title">{{ paper.display_name }}</span> </div>
+          <div  class="author" v-for="(author,index1) in paper.authorships" :key="index1">
+            <div class="paper-details">
+              <div   @mouseover="showAuthorInfo(author.author)"
+                     @mouseleave="hideAuthorInfo"
+                     class="author_container"
+              ><span id="authorName" class="author-name-hover">{{ author.author.display_name }}</span>
+                <span v-if="index1 !== paper.authorships.length - 1">，</span>
+              </div>
+            </div>
+            <transition  name="slide">
+              <div v-if="authorInfo && authorInfo.id === author.author.id" class="author-info">
+                <img src="@/assets/imgs/default.jpg" alt="Author Avatar">
+                <div class="author-details">
+                  <div class="author-name">{{ authorInfo.display_name }}</div>
+                  <div class="author-stats">
+                    引用量: {{ authorInfo.citations }}&nbsp; | &nbsp; 论文数: {{ authorInfo.paper_count }}
+                  </div>
+                  <div class="author-title">
+                    贡献： {{
+                      author.author_position === 'first' ? '第一作者' :
+                          author.author_position === 'middle' ? '中间作者' :
+                              author.author_position === 'last' ? '最后作者' :
+                                  '其他作者'
+                    }}
+                  </div>
+                  <div class="author-title">
+                  </div>
+                </div>
+              </div>
+            </transition>
+          </div>
+          <div v-if="paper.abstract" class="paper-abstract">
+            <div class="title">摘要</div>
+            <div class="abstract-text" v-if="paper.abstract.length<500">
+              {{paper.abstract}}
+            </div>
+            <div v-else>
+              <div v-if="showAbstract" class="abstract-text">
+                {{ paper.abstract }}
+                <span class='shouqi' @click="toggleAbstract">收起</span>
+              </div>
+              <div v-else class="abstract-text-collapsed">
+                {{ paper.abstract.slice(0, 500) }}...
+                <span class="more" @click="toggleAbstract">更多</span>
+              </div>
+            </div>
 
+          </div>
+          <div v-if="paper.keywords" class="keywords">
+            <div class="title">关键词</div>
+            <div class="keywords-text">
+              <div class="keyword"  v-for="(keyword,index1) in paper.keywords">
+                <span class="keyword-content"> {{keyword.keyword}}</span>
+                <span v-if="index1 !== paper.keywords.length - 1">，</span>
+              </div>
+            </div>
+          </div>
+          <div class="paper-stats">
+            引用: <span class="count">{{ paper.cited_by_count }}</span>
+          </div>
+          <div class="buttons">
+            <div @click="downloadPDF" @mouseover="showDownload" @mouseleave="hideDownload" class="download-pdf">
+              <span class="pdf"> PDF</span>
+              <transition  name="slide1">
+                <span class="arrow" v-if="download"><ArrowDownOutlined /></span>
+              </transition>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="reference_work">
+        <div class="title"> 参考文献</div>
+        <ReferenceWork :reference_works="reference_works"></ReferenceWork>
+      </div>
+    </div>
+    <div class="sideBar">
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.paper {
+  padding: 10px;
+  background-color: white;
+  border-radius: 10px !important;
+  text-align: left;
+  color: #363c50 !important;
+  box-shadow: rgba(99, 99, 99, 0.2) 0 2px 8px 0;
+}
 
+.paper h2 {
+  font-size: 24px;
+  font-weight: bold;
+  color: #5a5a5a;
+}
+
+.paper-item {
+  font-size: 18px;
+  padding: 10px;
+  color: #363c50;
+}
+
+.paper-title {
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: bold;
+  color: #000E28;
+}
+.paper-details {
+  cursor: pointer;
+  font-size: 14px;
+  color: #75a468;
+}
+
+.paper-abstract {
+  margin-bottom: 10px;
+  margin-top: 10px;
+  font-size: 14px;
+  line-height: 1.6;
+  color: #5a5a5a;
+}
+.keywords-text{
+  display: flex;
+}
+.author_container{
+  display: inline-block;
+}
+.title{
+  color: white;
+  width: 80%;
+  text-align: left;
+}
+.el-divider{
+  margin: 0 !important;
+  padding: 0 !important;
+}
+.author-container {
+  position: relative;
+}
+.author_container{
+  display: inline-block;
+}
+.title{
+  color: white;
+  width: 80%;
+  text-align: left;
+}
+.el-divider{
+  margin: 0 !important;
+  padding: 0 !important;
+}
+.author-container {
+  position: relative;
+}
+
+.author-info {
+  position: absolute;
+  border-radius: 5px;
+  top: auto;
+  transition: .5s;
+  margin-left: 10px;
+  background-color: #fff;
+  padding: 5px;
+  border: 1px solid #ccc;
+  display: flex;
+  box-shadow: 1px 1px #a0a5a8;
+}
+.author{
+  display: inline-block;
+}
+.author-info{
+  z-index: 10000;
+}
+.author-info img{
+  width: 80px;
+  height: 80px;
+  border-radius: 20px;
+}
+.slide-enter-from,
+.slide-leave-to{
+  opacity: 0;
+  transform: translateY(40px);
+}
+.slide-enter-active,
+.slide-leave-active{
+  transition: all .5s;
+}
+.author-name{
+  font-size: 20px;
+}
+.author-stats{
+  font-size: 12px;
+}
+.author-title{
+  font-size: 12px;
+}
+.author-details{
+  padding-right: 20px;
+  margin-left: 20px;
+}
+.paper-stats{
+  margin-bottom: 10px;
+  font-size: 14px;
+  color: #75a468;
+  font-weight: 600;
+}
+.count{
+  color: #75a468;
+}
+#authorName:hover{
+  text-decoration: none;
+  border-bottom: 1px dashed #75a468;
+}
+
+.slide-enter-from,
+.slide-leave-to{
+  opacity: 0;
+  transform: translateY(40px);
+}
+.slide-enter-active,
+.slide-leave-active{
+  transition: all .5s;
+}
+.slide1-enter-from,
+.slide1-leave-to{
+  opacity: 0;
+  transform: translateX(40px);
+}
+.slide1-enter-active,
+.slide1-leave-active{
+  transition: all .5s;
+}
+
+.recommendation-stats{
+  margin-bottom: 10px;
+  font-size: 14px;
+  color: #a0a5a8;
+}
+.title{
+  color:black;
+  font-size: 18px;
+  font-weight: 800;
+}
+.sideBar{
+  border-radius: 10px;
+  min-width: 300px;
+  /* 左侧导航栏样式 */
+  width: 20%; /* 左侧宽度，可以根据需求调整 */
+  background-color: #f0f1f4; /* 侧边栏背景色 */
+}
+.content{
+  margin-left: 10vw;
+  margin-top: 30px;
+  width: 60%;
+}
+.main-container{
+  min-height: 900px;
+  height: 100%;
+  background-color: #f0f1f4;
+  min-width:  1100px;
+  display: flex;
+}
+
+.shouqi{
+  cursor: pointer;
+  color: blue;
+}
+.more {
+  cursor: pointer;
+  color: blue;
+}
+.keyword{
+  font-size: 14px;
+  margin-right: 6px;
+  color: #666666;
+}
+/* 悬停添加箭头图标 */
+.download-pdf {
+  cursor: pointer;
+  display: flex;
+  border-radius: 10px;
+  background-color: #C51C01;
+  border: none;
+  color: #ffff;
+  text-align: center;
+  font-size: 15px;
+  font-weight: 400;
+  width: 70px;
+  transition: all 0.5s;
+  margin: 5px;
+  padding: 5px;
+  vertical-align: middle;
+}
+.download-pdf span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+.pdf{
+  font-weight: 600 ;
+  margin-left: 10px;
+}
+.arrow{
+  margin-left: 5px;
+}
+.reference_work {
+  margin-top: 20px;
+  background-color: white;
+  border-radius: 10px !important;
+  text-align: left;
+  color: #363c50 !important;
+  box-shadow: rgba(99, 99, 99, 0.2) 0 2px 8px 0;
+  padding: 20px;
+  margin-bottom: 20px;
+}
+
+.reference-item {
+  margin-bottom: 10px;
+  padding: 10px;
+  border-radius: 5px;
+}
+
+.title {
+  font-weight: bold;
+}
+
+.publication-year {
+  font-style: italic;
+}
 </style>
