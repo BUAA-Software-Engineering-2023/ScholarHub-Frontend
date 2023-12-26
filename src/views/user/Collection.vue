fc1d84552d039755ad93f7ebcb1300c1271baffb
Thebearcoding <1193617419@qq.com>
2023-12-26 17:34:50 +0800

    更新作者头像

--- src/views/user/Collection.vue
+++ src/views/user/Collection.vue
@@ -93,7 +93,7 @@ const isMounted = ref(false);
             </div>
             <transition  name="slide">
               <div v-if="authorInfo && authorInfo.id === author.author.id&&collection_id===collection.id" class="author-info">
-                <img src="@/assets/icons/default_avatar.png" alt="Author Avatar">
+                <img src="https://p4.itc.cn/q_70/images03/20230419/afa26485b395428abdc42e66a44d2e32.jpeg" alt="Author Avatar">
                 <div class="author-details">
                   <div class="author-name">{{ authorInfo.display_name }}</div>
                   <div class="author-stats">
